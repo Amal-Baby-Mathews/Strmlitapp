2889ed8a5f2713f69f00fc1ab9857fd7abc49eed
Amal-Baby-Mathews <amalbmat2001@gmail.com>
2023-12-15 10:11:09 +0530

    backup

--- import_str.py
+++ import_str.py
@@ -1,10 +1,12 @@
 import streamlit as st
 # from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import PyMuPDFLoader as PyMuPDF
+from langchain.document_loaders import PyPDFLoader
+from io import BytesIO
+from PyPDF2 import PdfReader, PdfWriter
 import spacy
 import time
-import os
-from uuid import uuid4
+# import os
+# from uuid import uuid4
 from pathlib import Path
 
 def save_path(template_file):
@@ -19,19 +21,58 @@ def save_path(template_file):
 
 # Function definitions
 def get_form_structure(file_path):
-    with PyMuPDF.open(file_path) as pdf:
-        form_fields = []
-        for page in pdf:
-            for field in page.form_fields:
-                form_fields.append({
-                    "name": field.name,
-                    "bbox": field.bbox,
-                    "type": field.type,
-                })
-    return form_fields
+  """
+  Extracts form structure from a PDF document.
+
+  Args:
+    file_path: Path to the PDF file.
+
+  Returns:
+    List of dictionaries containing information about each form field, including:
+      name: Field name.
+      bbox: Bounding box coordinates (left, bottom, right, top).
+      type: Field type (e.g., "text", "checkbox").
+
+  Raises:
+    IOError: If file cannot be opened.
+  """
+
+  with open(file_path, "rb") as f:
+    pdf_bytes = f.read()
+
+  # Convert file content to PdfReader object
+  reader = PdfReader(BytesIO(pdf_bytes))
+
+  form_fields = []
+  for page_num in range(len(reader.pages)):
+    # Get page dictionary
+    page = reader.pages[page_num]
+
+    # Check if page dictionary has "/AcroForm" field
+    if "/AcroForm" not in page:
+      continue
+
+    # Extract form fields for current page
+    page_fields = page["/AcroForm"]
+    for field_name in page_fields.keys():
+      # Get field dictionary
+      field = page_fields[field_name]
+
+      # Extract relevant information
+      bbox = field.get("Rect", [0, 0, 0, 0])
+      field_type = field.get("/T", "unknown")
+
+      form_fields.append({
+        "name": field_name,
+        "bbox": bbox,
+        "type": field_type,
+      })
+
+  return form_fields
 
 def extract_data(file_path):
-    nlp = spacy.load("en_core_web_lg")
+    #spacy.cli.download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
     with open(str(file_path), "r") as f:
         text = f.read().strip()
         doc = nlp(text)
@@ -50,17 +91,46 @@ def match_data_to_fields(form_fields, extracted_data):
     return matched_data
 
 def fill_form(template_path, data):
-    with PyMuPDF.open(template_path, "rb") as pdf:
-        writer = PyMuPDF.PdfWriter()
-        for page in pdf:
-            new_page = writer.addPage()
-            new_page.copyContentsFrom(page)
-            for field_name, field_value in data.items():
-                for field in page.form_fields:
-                    if field.name == field_name:
-                        field.setValue(field_value)
-                        break
-        writer.save("filled_form.pdf")
+  """
+  Fills a PDF form with provided data and saves the filled document.
+
+  Args:
+    template_path: Path to the PDF form template.
+    data: Dictionary mapping form field names to their corresponding values.
+
+  Returns:
+    None. Saves the filled PDF document as "filled_form.pdf".
+
+  Raises:
+    KeyError: If a data key doesn't match any field names in the form.
+  """
+
+  reader = PdfReader(template_path)
+  writer = PdfWriter()
+
+  for page_num in range(len(reader.pages)):
+    # Get page object
+    page = reader.pages[page_num]
+
+    # Create a new page and copy content from original page
+    new_page = writer.add_page(page)
+    new_page.merge_page(page)
+
+    # Loop through data and update matching form fields
+    for field_name, field_value in data.items():
+      found = False
+      for field in page["/AcroForm"].keys():
+        if field == field_name:
+          # Set field value if found
+          page["/AcroForm"][field].update({"V": field_value})
+          found = True
+          break
+
+      if not found:
+        raise KeyError(f"Field name '{field_name}' not found in the form.")
+
+  # Save the filled form as "filled_form.pdf"
+  writer.write("filled_form.pdf")
 
 # Streamlit app
 mystyle = '''
@@ -77,7 +147,7 @@ document_files = st.file_uploader("Relevant Documents (.txt)", type=["txt"], acc
 # Processing and results section
 if template_file and document_files:
     st.markdown("**The file is sucessfully Uploaded.**")
-    template=save_path(template_file)
+    template=str(save_path(template_file))
     # doc=save_path(document_files)
     progress_bar = st.empty()
     st.write("Analyzing documents...")
@@ -95,8 +165,8 @@ if template_file and document_files:
         st.write(f"Documents processed in {round(end_time - start_time, 2)} seconds.")
 
     # Display extracted entities
-    st.subheader("Extracted Entities ", style="font-size: 18px; color: #333")
-    st.table(extracted_data, header_style="font-weight: bold; color: #333", cell_style="text-align: left")
+    st.subheader("Extracted Entities ")
+    st.table(extracted_data)
 
     # Missing data and manual input
     missing_fields = [field for field in form_fields if field["name"] not in matched_data]
