74bfe6b4550eba316cff1b00812c67d72a4af9fd
Amal-Baby-Mathews <amalbmat2001@gmail.com>
2023-12-15 15:16:38 +0530

    good

--- import_str.py
+++ import_str.py
@@ -1,14 +1,18 @@
 import streamlit as st
+import fitz
 # from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import PyPDFLoader
 from io import BytesIO
 from PyPDF2 import PdfReader, PdfWriter
 import spacy
+from langchain.embeddings import HuggingFaceEmbeddings
 import time
 # import os
 # from uuid import uuid4
 from pathlib import Path
-
+import re
+from langchain.vectorstores import FAISS
+import requests
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 def save_path(template_file):
     save_folder = r'C:\Users\seq_amal\Strmlitapp\data'
     save_path = Path(save_folder, template_file.name)
@@ -89,49 +93,68 @@ def match_data_to_fields(form_fields, extracted_data):
                 matched_data[field["name"]] = entity["text"]
                 break
     return matched_data
+qAPI_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
+qheaders = {"Authorization": "Bearer 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"}
+def queryQ(payload):
+	response = requests.post(qAPI_URL, headers=qheaders, json=payload)
+	return response.json()
 
-def fill_form(template_path, data):
-  """
-  Fills a PDF form with provided data and saves the filled document.
+def fill_form(pdf_path, data):
+        model_name = "sentence-transformers/all-MiniLM-L6-v2"
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': False}
+        embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+        )
+        texts = [" "]
+        index = FAISS.from_texts(texts,embeddings)
+        # Initialize Faiss index and Hugging Face pipeline
+        
+        for document in data:
+            text_splitter = RecursiveCharacterTextSplitter(
+               chunk_size=100,
+               chunk_overlap=50,
+               )
+            documents = text_splitter.split_text(document["text"])
+            index.add_texts(documents)
+        
+        # Open PDF with PyMuPDF
+        doc = fitz.open(pdf_path)
 
-  Args:
-    template_path: Path to the PDF form template.
-    data: Dictionary mapping form field names to their corresponding values.
+        # Iterate through pages and lines
+        for page in doc:
+            blocks = page.get_text("blocks")
+            for block in blocks:
+                text = block[4]  # Get block text
 
-  Returns:
-    None. Saves the filled PDF document as "filled_form.pdf".
+                # Identify empty form line using regex
+                if re.match(r"(.*):\s*$", text):
+                    field_label = re.match(r"(.*):", text).group(1)  # Extract field label
+                    relevant_documents = index.similarity_search_with_relevance_scores(field_label)
+                    answer = []                   
+                    for document, relevance_score in relevant_documents:
+                        if relevance_score > 0.4:
+                            print(relevance_score,document.page_content)
+                            answer.append(document.page_content)
+                    answer = ' '.join(answer)
+                    # Semantic search using Faiss
+                    query = f"What is the value for {field_label}?"
+                    output = queryQ({
+        "inputs": {
+            "question": query,
+            "context": answer
+        },
+    })
+                    time.sleep(20)
+                    # Fill empty space with retrieved information
+                    filled_text = text.replace(":", ": " + output)
+                    page.insert_text((block[0], block[1], block[2], block[3]), filled_text)
 
-  Raises:
-    KeyError: If a data key doesn't match any field names in the form.
-  """
-
-  reader = PdfReader(template_path)
-  writer = PdfWriter()
-
-  for page_num in range(len(reader.pages)):
-    # Get page object
-    page = reader.pages[page_num]
-
-    # Create a new page and copy content from original page
-    new_page = writer.add_page(page)
-    new_page.merge_page(page)
-
-    # Loop through data and update matching form fields
-    for field_name, field_value in data.items():
-      found = False
-      for field in page["/AcroForm"].keys():
-        if field == field_name:
-          # Set field value if found
-          page["/AcroForm"][field].update({"V": field_value})
-          found = True
-          break
-
-      if not found:
-        raise KeyError(f"Field name '{field_name}' not found in the form.")
-
-  # Save the filled form as "filled_form.pdf"
-  writer.write("filled_form.pdf")
-
+        # Save filled PDF
+        doc.save("Filled_up.pdf")
+        doc.close()
 # Streamlit app
 mystyle = '''
 Hello nice to meet you
@@ -167,7 +190,6 @@ if template_file and document_files:
     # Display extracted entities
     st.subheader("Extracted Entities ")
     st.table(extracted_data)
-
     # Missing data and manual input
     missing_fields = [field for field in form_fields if field["name"] not in matched_data]
     if missing_fields:
@@ -179,7 +201,7 @@ if template_file and document_files:
 
     # Generate and download filled form
     if st.button("Generate Filled Form 🪄"):
-        fill_form(template, matched_data)
+        fill_form(template, extracted_data)
         st.success("Filled form generated! Download it here:")
         st.download_button("filled_form.pdf", "filled_form.pdf")
 
